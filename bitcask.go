1fa8fbf4236a77faa6b334dd299c76803966d528
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-09-17 21:25:17 +0300

    feat:  added open api, helper function read entries

--- bitcask.go
+++ bitcask.go
@@ -1,7 +1,11 @@
 package bitcask
 
 import (
+	"bufio"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -20,7 +24,6 @@ type KeydirEntry struct {
 	Timestamp uint64
 	FileId    string
 	ValueSize int
-	ValuePos  int64
 }
 
 type Options struct {
@@ -36,3 +39,68 @@ type BitcaskHandle struct {
 	active *os.File
 }
 
+func readRecords(f *os.File) ([]FileEntry, error) {
+	var entries []FileEntry
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		parts := strings.SplitN(scanner.Text(), " ", 6)
+
+		crc, _ := strconv.Atoi(parts[0])
+		ts, _ := strconv.ParseInt(parts[1], 10, 64)
+		ksz, _ := strconv.Atoi(parts[2])
+		vsz, _ := strconv.Atoi(parts[3])
+		key := parts[4]
+		value := parts[5]
+		entries = append(entries, FileEntry{
+			Crc:       uint32(crc),
+			Timestamp: uint64(ts),
+			KeySize:   ksz,
+			ValueSize: vsz,
+			Key:       key,
+			Value:     value,
+		})
+	}
+	return entries, scanner.Err()
+}
+
+func Open(dir string, opts Options) (*BitcaskHandle, error) {
+
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	b := &BitcaskHandle{
+		Dir:    dir,
+		Keydir: make(map[string]KeydirEntry),
+		Opts:   opts,
+	}
+
+	files, err := filepath.Glob(filepath.Join(dir, "*.data"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		for {
+			entries, err := readRecords(f)
+			if err != nil {
+				return nil, err
+			}
+			for _, entry := range entries {
+				b.Keydir[entry.Key] = KeydirEntry{
+					Timestamp: entry.Timestamp,
+					FileId:    filepath.Base(file),
+					ValueSize: entry.ValueSize,
+				}
+			}
+		}
+	}
+	return b, nil
+}
