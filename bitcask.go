fd28ebfd8b4deb6a94af8b5332195be71d28ed12
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-09-18 11:49:36 +0300

    fix: changed the open logic to use bytes instead of spaces and \ns

--- bitcask.go
+++ bitcask.go
@@ -1,15 +1,18 @@
 package bitcask
 
 import (
-	"bufio"
+	"encoding/binary"
+	"errors"
+	"hash/crc32"
+	"io"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 	"sync"
 )
 
 const MaxFileSize = 100 * 1024 * 1024 // 100MB
+
+var ErrIncorrectCrc = errors.New("incorrect crc")
 
 type FileEntry struct {
 	Crc       uint32
@@ -24,6 +27,7 @@ type KeydirEntry struct {
 	Timestamp uint64
 	FileId    string
 	ValueSize int
+	ValuePos  int64
 }
 
 type Options struct {
@@ -39,28 +43,47 @@ type BitcaskHandle struct {
 	active *os.File
 }
 
-func readRecords(f *os.File) ([]FileEntry, error) {
-	var entries []FileEntry
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		parts := strings.SplitN(scanner.Text(), " ", 6)
-
-		crc, _ := strconv.Atoi(parts[0])
-		ts, _ := strconv.ParseInt(parts[1], 10, 64)
-		ksz, _ := strconv.Atoi(parts[2])
-		vsz, _ := strconv.Atoi(parts[3])
-		key := parts[4]
-		value := parts[5]
-		entries = append(entries, FileEntry{
-			Crc:       uint32(crc),
-			Timestamp: uint64(ts),
-			KeySize:   ksz,
-			ValueSize: vsz,
-			Key:       key,
-			Value:     value,
-		})
+// -----header-----
+// crc	ts	ks	vs		key		value
+// 4	8	4	4		ksz		vsz
+func readRecord(f *os.File) (*FileEntry, int64, error) {
+	header := make([]byte, 20)
+	if _, err := io.ReadFull(f, header); err != nil {
+		return nil, 0, err
 	}
-	return entries, scanner.Err()
+
+	crc := binary.BigEndian.Uint32(header[0:4])
+	ts := binary.BigEndian.Uint64(header[4:12])
+	kSize := int(binary.BigEndian.Uint32(header[12:16]))
+	vSize := int(binary.BigEndian.Uint32(header[16:20]))
+
+	key := make([]byte, kSize)
+	if _, err := io.ReadFull(f, key); err != nil {
+		return nil, 0, err
+	}
+	val := make([]byte, vSize)
+	if _, err := io.ReadFull(f, val); err != nil {
+		return nil, 0, err
+	}
+
+	crcBytes := append(header[4:], key...)
+	crcBytes = append(crcBytes, val...)
+
+	if crc32.ChecksumIEEE(crcBytes) != crc {
+		return nil, 0, ErrIncorrectCrc
+	}
+
+	entry := &FileEntry{
+		Crc:       crc,
+		Timestamp: ts,
+		KeySize:   kSize,
+		ValueSize: vSize,
+		Key:       string(key),
+		Value:     val,
+	}
+
+	totalSize := int64(len(header) + kSize + vSize)
+	return entry, totalSize, nil
 }
 
 func Open(dir string, opts Options) (*BitcaskHandle, error) {
@@ -88,18 +111,20 @@ func Open(dir string, opts Options) (*BitcaskHandle, error) {
 		}
 		defer f.Close()
 
+		var offset int64
 		for {
-			entries, err := readRecords(f)
+			entry, size, err := readRecord(f)
 			if err != nil {
 				return nil, err
 			}
-			for _, entry := range entries {
-				b.Keydir[entry.Key] = KeydirEntry{
-					Timestamp: entry.Timestamp,
-					FileId:    filepath.Base(file),
-					ValueSize: entry.ValueSize,
-				}
+			b.Keydir[entry.Key] = KeydirEntry{
+				Timestamp: entry.Timestamp,
+				FileId:    filepath.Base(file),
+				ValueSize: entry.ValueSize,
+				ValuePos:  offset + 20 + int64(entry.KeySize), // to stop right before the value
 			}
+
+			offset += size
 		}
 	}
 	return b, nil
